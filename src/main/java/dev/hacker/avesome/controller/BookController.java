df8fc1575ab41e1ee96b339cbb828e6101dd8707
Natashka <natashikar@gmail.com>
2023-06-10 14:22:54 +0300

    examen JEE

--- src/main/java/dev/hacker/avesome/controller/BookController.java
+++ src/main/java/dev/hacker/avesome/controller/BookController.java
@@ -2,11 +2,13 @@ package dev.hacker.avesome.controller;
 
 import dev.hacker.avesome.model.Book;
 import dev.hacker.avesome.repository.BookRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -28,4 +30,12 @@ public class BookController {
         return bookRepository.save(book);
     }
 
+    @PostConstruct
+    private void init(){
+        bookRepository.saveAll(List.of(new Book("9789664668320","Enciclopedie in intrebari si raspunsuri", LocalDate.parse("2000-07-10"),"Pegas",100),
+                new Book("String isbn","String name", LocalDate.now(),"String editura",200),
+                new Book("String isbn","String name", LocalDate.now(),"String editura",300),
+                new Book("String isbn","String name", LocalDate.now(),"String editura",400),
+                new Book("String isbn","String name", LocalDate.now(),"String editura",500)));
+    }
 }
--- src/main/java/dev/hacker/avesome/model/Book.java
+++ src/main/java/dev/hacker/avesome/model/Book.java
@@ -3,17 +3,47 @@ package dev.hacker.avesome.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+
+import java.time.LocalDate;
 
 @Entity
+
 public class Book {
     @Id
     @GeneratedValue
     private Long id;
-
+    private String isbn;
     private String name;
+    private LocalDate publicationYear;
+    private String editura;
+    private long n_page;
+
+    public Book() {
+    }
+
+    public Book(String isbn, String name, LocalDate publicationYear, String editura, long n_page) {
+        this.isbn = isbn;
+        this.name = name;
+        this.publicationYear = publicationYear;
+        this.editura = editura;
+        this.n_page = n_page;
+    }
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 
     public String getName() {
@@ -22,5 +52,29 @@ public class Book {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDate getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(LocalDate publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getEditura() {
+        return editura;
+    }
+
+    public void setEditura(String editura) {
+        this.editura = editura;
+    }
+
+    public long getN_page() {
+        return n_page;
+    }
+
+    public void setN_page(long n_page) {
+        this.n_page = n_page;
     }
 }
